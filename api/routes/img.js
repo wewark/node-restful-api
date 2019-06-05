69c33533f3a60542b5dc468c283b979df926ba33
Khaled Hamed <khaled.hamedt@gmail.com>
2019-06-05 23:22:51 +0200

    move token to header

--- api/routes/img.js
+++ api/routes/img.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const jwt = require('jsonwebtoken')
 const checkAuth = require('../middleware/check-auth')
 
 router.post('/create_thumb', checkAuth, (req, res, next) => {
-    console.info('generating thumb');
-    
+    console.log('generating thumb');
+    // TODO
     return res.status(200).json({
         message: "Auth successful",
     })
