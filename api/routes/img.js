c8c3637b2eeb4ae1b9e190e2e0a98b56fbb999c3
Khaled Hamed <khaled.hamedt@gmail.com>
2019-06-06 17:37:23 +0200

    resize img

--- api/routes/img.js
+++ api/routes/img.js
@@ -1,13 +1,33 @@
 const express = require('express')
 const router = express.Router()
 const checkAuth = require('../middleware/check-auth')
+const jimp = require('jimp')
+const fs = require('fs')
 
 router.post('/create_thumb', checkAuth, (req, res, next) => {
-    console.log('generating thumb');
-    // TODO
-    return res.status(200).json({
-        message: "Auth successful",
-    })
+    const thumb_name = process.cwd() + '/temp/thumb'
+    const thumb_height = 50
+    const thumb_width = 50
+
+    jimp.read(req.query.img_url)
+        .then(image => {
+            image
+                .resize(thumb_height, thumb_width)
+                .write(thumb_name, (err) => {
+                    if (err) {
+                        next(err)
+                    } else {
+                        res.setHeader('Content-Type', 'image/jpeg')
+                        res.sendFile(thumb_name, (err) => {
+                            // Delete thumb
+                            fs.unlinkSync(thumb_name)
+                        })
+                    }
+                })
+        })
+        .catch(err => {
+            next(err)
+        })
 })
 
 module.exports = router
