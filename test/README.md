8de9bdabab24784642b412212a38d13470841d54
Khaled Hamed <khaled.hamedt@gmail.com>
2019-06-18 22:22:02 +0200

    add json patcher testcases

--- test/README.md
+++ test/README.md
@@ -2,13 +2,13 @@
 
 File                             |  % Stmts | % Branch |  % Funcs |  % Lines | Uncovered Line #s |
 ---------------------------------|----------|----------|----------|----------|-------------------|
-All files                        |    83.56 |       50 |       60 |    83.56 |                   |
+All files                        |    89.04 |       50 |       70 |    89.04 |                   |
  node-restful-api                |    79.31 |       50 |        0 |    79.31 |                   |
   app.js                         |    71.43 |       25 |        0 |    71.43 | 12,22,23,24,29,30 |
   server.js                      |      100 |    66.67 |      100 |      100 |               1,9 |
  node-restful-api/api/middleware |      100 |      100 |      100 |      100 |                   |
   check-auth.js                  |      100 |      100 |      100 |      100 |                   |
- node-restful-api/api/routes     |    83.33 |       50 |    71.43 |    83.33 |                   |
+ node-restful-api/api/routes     |    94.44 |       50 |    85.71 |    94.44 |                   |
   img.js                         |    88.89 |       50 |       80 |    88.89 |             18,29 |
-  json.js                        |       60 |      100 |        0 |       60 |         8,9,10,11 |
+  json.js                        |      100 |      100 |      100 |      100 |                   |
   user.js                        |      100 |      100 |      100 |      100 |                   |
--- test/json.js
+++ test/json.js
@@ -5,31 +5,63 @@ const chaiHttp = require('chai-http')
 const server = require('../server')
 const expect = chai.expect
 
-const fs = require('fs')
-const JPEG = require('jpeg-js')
-const pixelmatch = require('pixelmatch')
-
 
 chai.use(chaiHttp)
 
 describe('/POST /json/patch', () => {
     const username = 'khaled'
     const password = 'password123'
-    const document = {
-        name: 'khaled',
-        power: 100,
-    }
-    const patch = {
-        // op: 'replace', // TODO
-    }
-    const exp_doc = document // TODO
-    
+
+    const testcases = [
+        {
+            document: {
+                "baz": "qux",
+                "foo": "bar"
+            },
+            patch: [
+                { "op": "replace", "path": "/baz", "value": "boo" },
+                { "op": "add", "path": "/hello", "value": ["world"] },
+                { "op": "remove", "path": "/foo" }
+            ],
+            exp_result: {
+                "baz": "boo",
+                "hello": ["world"]
+            },
+        },
+        {
+            document: {
+                "name": "khaled"
+            },
+            patch: [],
+            exp_result: {
+                "name": "khaled"
+            },
+        },
+        {
+            document: {
+                "name": "khaled"
+            },
+            patch: [
+                { "op": "replace", "path": "/name", "value": "hamed" },
+                { "op": "add", "path": "/power", "value": 100 },
+                { "op": "add", "path": "/arr", "value": { "okay": true } }
+            ],
+            exp_result: {
+                "name": "hamed",
+                "power": 100,
+                "arr": {
+                    "okay": true
+                }
+            },
+        }
+    ]
+
     it('rejects when not logged in', done => {
         chai.request(server)
             .post('/json/patch')
             .send({
-                document: document,
-                patch: patch,
+                document: JSON.stringify(testcases[0].document),
+                patch: JSON.stringify(testcases[0].patch),
             })
             .end((err, res) => {
                 expect(err).null
@@ -52,8 +84,8 @@ describe('/POST /json/patch', () => {
                     .set('content-type', 'application/x-www-form-urlencoded')
                     .set('Authorization', 'Bearer ' + res.body.token)
                     .send({
-                        document: JSON.stringify(document),
-                        patch: JSON.stringify(patch),
+                        document: JSON.stringify(testcases[0].document),
+                        patch: JSON.stringify(testcases[0].patch),
                     })
                     .end((err, res) => {
                         expect(err).null
@@ -63,29 +95,39 @@ describe('/POST /json/patch', () => {
             })
     })
 
-    it('successfully patches document', done => {
-        chai.request(server)
-            .post('/user/login')
-            .set('content-type', 'application/x-www-form-urlencoded')
-            .send({
-                username: username,
-                password: password
-            })
-            .end((err, res) => {
+    describe('Patching', () => {
+        let authToken = ''
+        before('login', done => {
+            chai.request(server)
+                .post('/user/login')
+                .set('content-type', 'application/x-www-form-urlencoded')
+                .send({
+                    username: username,
+                    password: password
+                })
+                .end((err, res) => {
+                    authToken = res.body.token
+                    done()
+                })
+        })
+
+        testcases.forEach((testcase, i) => {
+            it('successfully patches document ' + (i + 1), done => {
                 chai.request(server)
                     .post('/json/patch')
                     .send({
-                        document: JSON.stringify(document),
-                        patch: JSON.stringify(patch),
+                        document: JSON.stringify(testcase.document),
+                        patch: JSON.stringify(testcase.patch),
                     })
-                    .set('Authorization', 'Bearer ' + res.body.token)
+                    .set('Authorization', 'Bearer ' + authToken)
                     .set('content-type', 'application/x-www-form-urlencoded')
                     .end((err, res) => {
                         expect(err).null
                         expect(res).status(200)
-                        expect(res.body.document).eql(exp_doc)
+                        expect(res.body.document).eql(testcase.exp_result)
                         done()
                     })
             })
+        })
     })
 })
